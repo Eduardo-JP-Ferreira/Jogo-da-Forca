5d55fd9cade88bd3105b450e431c996de2b6f3e4
Eduardo-JP-Ferreira <eduardopires2014@gmail.com>
2023-02-26 21:25:36 -0300

    Sorteio da Palavra Implementado

--- src/components/Jogo.js
+++ src/components/Jogo.js
@@ -1,31 +1,46 @@
 import { useState } from "react";
 import Letras from "./Letras"
-export default function Jogo(){
+import palavras from "../palavras";
+
+export default function Jogo(props){
 
     const imagens = ["assets/forca0.png","assets/forca1.png","assets/forca2.png",
     "assets/forca3.png","assets/forca4.png","assets/forca5.png","assets/forca6.png"]
 
     const [habilitar, setHabilitar] = useState("0")
+    const [erros, setErros] = useState(0)
     function escolherPalavra(){
         setHabilitar("1");
     }
-    
+    const elemento = palavras[Math.floor(Math.random() * palavras.length)]
+    const arrayPalavra = elemento.split("")
+    console.log(arrayPalavra)
+    const arrayUnderline=[]
+    for(let i=0;i<arrayPalavra.length;i++){
+        arrayUnderline.push("_ ")
+    }
+    console.log(arrayUnderline)
+
+    console.log("value",props.value)
+
     return(
         <div className="containerTotal">
             <div className="container">
                 <div className="imagemForca">
-                    <img src={imagens[0]}></img>
+                    <img src={imagens[erros]}></img>
                 </div>
                 <div className="direita">
                     <div className="botaoEscolherPalavra" onClick={escolherPalavra}>
                         <p>Escolher Palavra</p>
                     </div>
                     <div className="palavra">
-                        <p>PALAVRA</p>
+                        {habilitar === "0" ? 
+                        (<p className="invisivel">{arrayUnderline}</p>) : 
+                        (<p>{arrayUnderline}</p>)}
                     </div>
                 </div>
             </div>
-            <Letras habilitado ={habilitar}/>
+            <Letras habilitado ={habilitar} array = {arrayPalavra}/>
         </div>
     )
 }
--- src/components/Letras.js
+++ src/components/Letras.js
@@ -1,22 +1,35 @@
-
-
+import Jogo from "./Jogo";
+import { useState } from "react";
 export default function Letras(props){
     const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
     "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     let a;
-
-    function p(){
-        alert("Letra")
+    // const [dado, setDado] = useState("0")
+    const p = (a) => {
+        const nv = props.array
+        if(nv.includes(a)){
+            alert("Tem")
+            // setDado("1")
+            // {<Jogo value= {dado}/>}
+            
+        }
+        else{
+            alert("Não tem")
+            // setDado("0")
+            // {<Jogo value= {dado}/>}
+            
+        }
+        
     }
     return(
         <div className="containerLetras">
             {alfabeto.map((item)=>
-    
+                
                 <div className="letras">
                     {props.habilitado === "0" ? (
-                        <button type="button" onClick={p} disabled>{item}</button>
+                        <button type="button" value={item} onClick={() => p(item) } disabled><span>{item}</span></button>
                     ) : (
-                    <button type="button" onClick={p} >{item}</button>)}
+                    <button type="button" onClick={() => p(item)} >{item}</button>)}
                 </div>
             )}
         </div>
