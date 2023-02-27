8ea5f4ca14d4acc8460fa7ebd637ce6c20578bf9
Eduardo-JP-Ferreira <eduardopires2014@gmail.com>
2023-02-27 06:02:21 -0300

    Upgrade Habilitação dos Botões

--- src/components/App.js
+++ src/components/App.js
@@ -5,84 +5,107 @@ import { useState } from "react";
 import palavras from "../palavras";
 
 function App() {
-    const imagens = ["assets/forca0.png","assets/forca1.png","assets/forca2.png",
-    "assets/forca3.png","assets/forca4.png","assets/forca5.png","assets/forca6.png"]
+  const imagens = ["assets/forca0.png", "assets/forca1.png", "assets/forca2.png",
+    "assets/forca3.png", "assets/forca4.png", "assets/forca5.png", "assets/forca6.png"]
 
-    const [habilitar, setHabilitar] = useState("0")
-    const [erros, setErros] = useState(0)
-    const [arrayUnderline, setArrayUnderline] = useState([])
-    const [elemento, setElemento] = useState("")
-    const [arrayPalavra, setArrayPalavra] = useState([])
-    //const arrayPalavra = [""];
+  const [habilitarLetras, setHabilitarLetras] = useState("0")
+  const [habilitar, setHabilitar] = useState("0")
+  const [erros, setErros] = useState(0)
+  const [arrayUnderline, setArrayUnderline] = useState([])
+  const [elemento, setElemento] = useState("")
+  const [arrayPalavra, setArrayPalavra] = useState([])
+  const [letrasClicadas, setLetrasClicadas] = useState([])
+  const [qntLetras, setQntLetras] = useState(0)
+  const [valorCor, setValorCor] = useState(0)
+  //const arrayPalavra = [""];
 
-    console.log("ComeçoUNder",arrayUnderline)
-    function escolherPalavra(){
-      const elemento2 = palavras[Math.floor(Math.random() * palavras.length)]
-      console.log("palavra",elemento2)
-      setElemento(elemento2)
-      console.log("palavra2",elemento2)
+  console.log("ComeçoUNder", arrayUnderline)
+  function escolherPalavra() {
+    const elemento2 = palavras[Math.floor(Math.random() * palavras.length)]
+    console.log("palavra", elemento2)
+    setElemento(elemento2)
+    console.log("palavra2", elemento2)
 
-      const arrayzin = elemento2.split("")
+    const arrayzin = elemento2.split("")
 
-      console.log("arayzin",arrayzin)
-      console.log("under",arrayUnderline)
-      for(let i=0;i<arrayzin.length;i++){
-        arrayUnderline.push("_ ")
-        arrayPalavra[i]=arrayzin[i]
-      }
-      console.log("under2",arrayUnderline)
-      console.log("AApalavra",arrayPalavra)
-
-       setHabilitar("1");
+    console.log("arayzin", arrayzin)
+    console.log("under", arrayUnderline)
+    for (let i = 0; i < arrayzin.length; i++) {
+      arrayUnderline.push("_ ")
+      arrayPalavra[i] = arrayzin[i]
     }
+    console.log("under2", arrayUnderline)
+    console.log("AApalavra", arrayPalavra)
 
-    function clique(letra){
-      console.log("CLICOU")
-      console.log("AApalavra",arrayPalavra)
-      console.log("UNder",arrayUnderline)
-      const pegaIndice = []
-      
-      if(arrayPalavra.includes(letra)){
-        console.log("Letra",letra) 
-        let indice = arrayPalavra.indexOf(letra)
-        console.log("Indice",indice)
+    setHabilitar("1");
+    setHabilitarLetras("1")
+  }
 
-        while(indice !== -1){
-          pegaIndice.push(indice)
-          indice = arrayPalavra.indexOf(letra, indice + 1)
-          //pega todas as letras repetidas e coloca no array
-        }
-        console.log("IndiceAP",pegaIndice)
+  function clique(letra) {
+    let contadorLetras = qntLetras
+    console.log("CLICOU")
+    console.log("AApalavra", arrayPalavra)
+    console.log("UNder", arrayUnderline)
 
-        for(let i = 0; i< pegaIndice.length; i++){
-          arrayUnderline[pegaIndice[i]]=letra
-          const novoArray = [...arrayUnderline]
-          setArrayUnderline(novoArray)
-        }
-        console.log("novo",arrayUnderline)
+    letrasClicadas.push(letra)
+    console.log("LETRA CL", letrasClicadas)
+    const pegaIndice = []
+
+    if (arrayPalavra.includes(letra)) {
+      console.log("Letra", letra)
+      let indice = arrayPalavra.indexOf(letra)
+      console.log("Indice", indice)
+
+      while (indice !== -1) {
+        pegaIndice.push(indice)
+        indice = arrayPalavra.indexOf(letra, indice + 1)
+        //pega todas as letras repetidas e coloca no array
       }
-      else{
-        setErros(erros +1)  
-      }
-    } 
+      console.log("IndiceAP", pegaIndice)
 
-    
-    // console.log(arrayUnderline)
-    return (
-      <div className="containerTotal">
-        <Jogo 
+      for (let i = 0; i < pegaIndice.length; i++) {
+        arrayUnderline[pegaIndice[i]] = letra
+        const novoArray = [...arrayUnderline]
+        setArrayUnderline(novoArray)
+        contadorLetras = contadorLetras + 1
+        console.log("LETRAS QNT", contadorLetras)
+      }
+      setQntLetras(contadorLetras)
+      if(contadorLetras == arrayPalavra.length){
+        setHabilitarLetras("0");
+        alert("Venceu!")
+      }
+      console.log("novo", arrayUnderline)
+    }
+    else if (erros < 5) {
+      setErros(erros + 1)
+    }
+    else {
+      setErros(erros + 1)
+      setHabilitarLetras("0");
+      alert("PERDEU")
+    }
+  }
+
+
+  // console.log(arrayUnderline)
+  return (
+    <div className="containerTotal">
+      <Jogo
         escolherPalavra={escolherPalavra}
         imagens={imagens} erros={erros}
         habilitar={habilitar} setHablitar={setHabilitar}
-        arrayUnderline={arrayUnderline}/>
+        arrayUnderline={arrayUnderline} />
 
-        <Letras
-        habilitado ={habilitar}
+      <Letras
+        habilitado={habilitarLetras}
         clique={clique}
         erros={erros}
-        setErros={setErros}/>
-        </div>
-    );
+        setErros={setErros}
+        valorCor ={valorCor}
+        letrasClicadas={letrasClicadas} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Letras.js
+++ src/components/Letras.js
@@ -6,16 +6,14 @@ export default function Letras(props){
     const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
     "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-
     return(
         <div className="containerLetras">
             {alfabeto.map((item)=>
                 
                 <div className="letras">
-                    {props.habilitado === "0" ? (
-                        <button data-test="letter" key={item} type="button" value={item} onClick={() => props.clique(item) } disabled><span>{item}</span></button>
-                    ) : (
-                    <button data-test="letter" key={item} type="button" onClick={() => props.clique(item)} >{item}</button>)}
+                    <button data-test="letter" key={item} type="button"
+                    disabled={(props.letrasClicadas.includes(item) || props.habilitado === "0") ? true:false}
+                    onClick={() => props.clique(item)} >{item}</button>
                 </div>
             )}
         </div>
